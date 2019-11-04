539ca357109d6f94a94b8a8b57743c79afcac8d0
Gottfried Chen <gottfried.chen@allaboutapps.at>
2019-11-04 10:55:44 +0100

    fix unknown types in JOI schema

--- src/api/user/_types.ts
+++ src/api/user/_types.ts
@@ -8,14 +8,21 @@ export const UserLegal = JOI.object().required().keys({
 // AppUserProfile builds on top of legal scheme, for easier clientside consumption
 export const AppUserProfile = UserLegal.required().keys({
     uid: types.uidv4.required(),
-    scope: JOI.array().items(JOI.string()).required().description("The scope (roles & permissions) of this user.")
-    // ... your custom fields
+    scope: JOI.array().items(JOI.string()).required().description("The scope (roles & permissions) of this user."),
+    data: JOI.object().optional().keys({
+        likedBeerIds: JOI.array().optional().items(JOI.number()),
+        commentsMap: JOI.object().optional().unknown(true)
+    })
 }).label("AppUserProfile").description("Provides all profile information about an AppUser");
 
 export const AppUserProfilePatchRequest = JOI.object().required().unknown(true).keys({
     // define the allowed keys to patch here... e.g.:
     // email: types.email.optional().allow(null).description("set to null to remove, no empty string allowed"),
     // phone: Joi.string().optional().allow(null).max(20).trim().description("null to remove, no empty string allowed")
+    data: JOI.object().optional().keys({
+        likedBeerIds: JOI.array().optional().items(JOI.number()),
+        commentsMap: JOI.object().optional().unknown(true)
+    })
 }).label("AppUserProfilePatchRequest")
     .description("Available fields to directly patch an AppUserProfile, unknown keys are allowed (whole modified AppUserProfile can be sent), but dismissed.");
 
