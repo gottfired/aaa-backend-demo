7720fb0e5d38006ce0e03791b130fe079a20dd6e
Gottfried Chen <gottfried.chen@allaboutapps.at>
2019-11-04 10:40:39 +0100

    TDD: add patch call test

---
+++ src/api/user/user.controller.test.ts
@@ -0,0 +1,24 @@
+import { expect } from "@aaa-backend-stack/test-environment";
+import Request from "../../test/Request";
+
+describe("PATCH /api/v1/user/profile", function () {
+
+    it.only("patch beer data", async () => {
+
+        const data = {
+            likedBeerIds: [1, 2, 3],
+            commentsMap: {
+                1: "comment1",
+                5: "comment5"
+            }
+        };
+
+        const res = await Request.create("PATCH", "/api/v1/user/profile", { user: "user1" }).send({
+            data: data
+        });
+
+        expect(res).to.have.status(200);
+        expect(res.body.data).to.eql(data);
+
+    });
+});
